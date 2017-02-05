054e7445cb7712d373e0a5584c1bad3b80c389d2
matthew goodwin <matthewjgoodwin@gmail.com>
2017-02-05 14:00:24 +0900

    pages, view/contrller and changes made routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def search
+  end
+
+  def shell
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'search', to: 'pages#search'
+  get 'shell', to: 'pages#shell'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
 end
