faeb2086c9e77d786281dae37aa1e3c6b5dd48f1
matthew goodwin <matthewjgoodwin@gmail.com>
2017-03-27 11:45:41 +0900

    fix on gemfile; heroku works..validations and navbar..

--- app/models/lounge.rb
+++ app/models/lounge.rb
@@ -3,4 +3,6 @@ class Lounge < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :bevs, dependent: :destroy
   validates :name, :cat, :desc, :city, presence: true
+  validates :name, uniqueness: {scope: :city, message: "this lounge name and city already exists!"}
+  # ^ should change this to address, not city
 end
