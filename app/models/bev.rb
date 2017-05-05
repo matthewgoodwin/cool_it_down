fd18e9b6aaaef2ddb80561491fb5c74dfafa601d
matthew goodwin <matthewjgoodwin@gmail.com>
2017-05-05 14:50:10 +0900

    bev and lounge model files

--- app/models/bev.rb
+++ app/models/bev.rb
@@ -2,6 +2,7 @@ class Bev < ApplicationRecord
   belongs_to :lounge
   has_many :orders, dependent: :destroy
   validates :name, :desc, :style, :cost, presence: true
+  validates :name, length: {maximum: 30, too_long: "%{count} characters is the maximum allowed"}
   # validates :name, uniqueness: { scope: :desc}
   # ^ restricted to user or db
 end
